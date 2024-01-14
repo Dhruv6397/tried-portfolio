b5cdbd76d4ef81fc9d2a2d5e8429c6a724c36907
Dhruv6397 <vermadhruv6397@gmail.com>
2024-01-14 07:16:58 +0530

    added the side bar and menu icon but not working right now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 import logo from './logo.png'
+import MenuIcon from '@mui/icons-material/Menu';
+// import CloseIcon from '@mui/icons-material/Close';
 export default class Navbar extends Component {
+  handleClick=()=>{
+
+  };
   render() {
+    
     return (
       <>
+        
+       
+        <ul className='side-bar'>
+        <li ><Link className="sidebar-link" to="/">Home</Link></li >
+        <li ><Link className="sidebar-link" to="/about">About</Link></li>
+        <li ><Link className="sidebar-link" to="/blog">Projects</Link></li>
+        <li><Link className="sidebar-link" to="/blog">Blog</Link></li>
+        </ul>
+        
+
         <nav>
         <Link to='/'> <img src={logo} alt="Logo" className="logo" /></Link>
         <ul className='nav-list'>
@@ -20,6 +36,9 @@ export default class Navbar extends Component {
         </li>
         <li className='list-items blog-class'>
           <Link className='link' to="/blog">Blog</Link>
+        </li>
+        <li onClick={this.handleClick} className='menu-icon'>
+          <a href='/'><MenuIcon/></a>
         </li>
         </ul>
         </nav>
