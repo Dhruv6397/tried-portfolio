98648e391632439105c41e33c402ad55723072d4
Dhruv6397 <vermadhruv6397@gmail.com>
2024-01-17 17:21:11 +0530

    theme changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import MyPic from './MyPic.svg';
 export default class Home extends Component {
   render() {
     return (
@@ -15,18 +16,23 @@ export default class Home extends Component {
                 <h5 className='txt'>Hey! there</h5>
                 <h4 className='txt'>I am </h4>
                 <h1 className='txt'>Dhruv</h1>
-                <p className='txt'><span className='nwrap'>Part time student,</span> <span className='nwrap'> Full time developer.</span></p>
+                <p className='txt'><span className='nwrap'>Part time student,</span> <br/><span className='nwrap'> Full time developer.</span></p>
             </div>
+            <div className='MyPicClass'>
+            <img src={MyPic} alt='My Profile pic'></img>
+          </div>
           </section> 
+          
 
           <div className='icons'>
+              <a href="https://github.com/dhruv6397" target='_blank' rel='noreferrer'><GitHubIcon className='media-icons  github'/></a>
+              <a href='https://www.linkedin.com/in/dhruv-8a01a5214/' target='_blank' rel='noreferrer'><LinkedInIcon className='media-icons linkedin'/></a>
+              <a href='https://www.twitter.com/dhruvverma____' target='_blank' rel='noreferrer'><TwitterIcon className='media-icons twitter'/></a>
               <a href='https://www.instagram.com/dhruv.verma___/' target='_blank' rel='noreferrer'><InstagramIcon className='media-icons instagram'/></a>
               <a href='https://www.youtube.com/@engineer.verma_' target='_blank' rel='noreferrer'><YouTubeIcon className='media-icons youtube'/></a>
-              <a href="https://github.com/dhruv6397" target='_blank' rel='noreferrer'><GitHubIcon className='media-icons  github'/></a>
-              <a href='/' target='_blank' rel='noreferrer'><LinkedInIcon className='media-icons linkedin'/></a>
-              <a href='/' target='_blank' rel='noreferrer'><TwitterIcon className='media-icons twitter'/></a>
-            
+              
           </div>
+          
         </div>
       </>
     )
